568b9f051255e7831729d35a75abfb7ee2d16408
Corwin Buzzard <corwin1998@gmail.com>
2020-05-18 13:35:15 -0400

    increased functionality

---
+++ exercises/exercises_out/SpaceLocation.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Paste in the provided code here:
+class SpaceLocation {
+    constructor(name, kilometersAway) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+}
+exports.SpaceLocation = SpaceLocation;
+//# sourceMappingURL=SpaceLocation.js.map
